310be20a33ba80953b07a198f3038417cc7f8db9
Jerrold Thompson <jet@whidbey.com>
2013-01-05 12:51:23 -0800

    Remove require spree_core in spree_mail_chimp.css

--- lib/generators/spree_mail_chimp/install/install_generator.rb
+++ lib/generators/spree_mail_chimp/install/install_generator.rb
@@ -7,7 +7,7 @@ module SpreeMailChimp
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_mail_chimp\n", :before => /\*\//, :verbose => true
+        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_mail_chimp\\\\n", :before => /\\\\*\\\\//, :verbose => true
       end
 
       def add_migrations
