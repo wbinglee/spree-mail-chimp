a85be2cb60c3084af064abefffff59cdedb0fd73
Tobias Bohwalli <hi@futhr.io>
2013-06-01 05:13:55 +0200

    Replated TestUnit with RSpec.
    - It still has no real tests but now it follow Spree testing pattern.
    - Add SimpleCov and Coveralls.
    - Fuubar for test runs.

---
+++ spec/requests/mailchimp_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "MailChimp" do
+  # before do
+  #   # create(:user)
+  # end
+
+  # context "" do
+  #   File.join(File.expand_path(Rails.root), 'spree_mail_chimp') eq MailChimp.root
+  #   'Mail Chimp' eq MailChimp.name
+  # end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,31 +1,54 @@
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'fabrication'
-
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  config.mock_with :rspec
-
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  #config.include Devise::TestHelpers, :type => :controller
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, comment the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+if ENV["COVERAGE"]
+  require 'simplecov'
+  require 'coveralls'
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+  SimpleCov.start do
+    add_filter '/spec/'
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Models', 'app/models'
+    add_group 'Overrides', 'app/overrides'
+    add_group 'Views', 'app/views'
+    add_group 'Libraries', 'lib'
+  end
 end
 
-@configuration ||= AppConfiguration.find_or_create_by_name("Default configuration")
+ENV['RAILS_ENV'] = 'test'
+
+require File.expand_path('../dummy/config/environment.rb',  __FILE__)
+require 'rspec/rails'
+require 'capybara/rspec'
+require 'ffaker'
+require 'database_cleaner'
+
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
+
+require 'spree/testing_support/factories'
+require 'spree/testing_support/url_helpers'
+
+RSpec.configure do |config|
+  config.include Capybara::DSL, type: :request
+  config.include FactoryGirl::Syntax::Methods
+  config.include Spree::TestingSupport::UrlHelpers
+
+  config.color = true
+  config.mock_with :rspec
+  config.use_transactional_fixtures = false
+  config.fail_fast = ENV['FAIL_FAST'] || false
+
+  config.before do
+    if example.metadata[:js]
+      DatabaseCleaner.strategy = :truncation
+    else
+      DatabaseCleaner.strategy = :transaction
+    end
+
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
+end
